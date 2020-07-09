9fc366ab5e4ded8dce27eb5ac1b469cdbb2c43a1
rohittir <rohit.tir2@gmail.com>
2020-07-09 08:52:47 -0400

    improved data display in tables

--- src/app/covid19/country-wise/country-wise.component.ts
+++ src/app/covid19/country-wise/country-wise.component.ts
@@ -47,14 +47,32 @@ export class CountryWiseComponent implements OnInit {
     getIndiaCasesData = (data: any) => {
         return Object.keys(data).map((key) => ({
             state: key,
+            ...this.getTotalIndiaStateData(data[key]),
             districts: Object.keys(data[key].districtData).map((distKey) => ({
                 name: distKey,
                 cases: data[key].districtData[distKey].confirmed + data[key].districtData[distKey].delta.confirmed,
                 deaths: data[key].districtData[distKey].deceased + data[key].districtData[distKey].delta.deceased,
                 active: data[key].districtData[distKey].active,
                 recovered: data[key].districtData[distKey].recovered + data[key].districtData[distKey].delta.recovered,
-            })),
-        }));
+            })).sort((a, b) => b.cases - a.cases),
+        })).sort((a, b) => b.cases - a.cases);
+    }
+
+    getTotalIndiaStateData = (stateData: any) => {
+        const stateObject = {
+            cases: 0,
+            deaths: 0,
+            active: 0,
+            recovered: 0
+        };
+        Object.keys(stateData.districtData).forEach(districtKey => {
+            stateObject.cases += (stateData.districtData[districtKey].confirmed + stateData.districtData[districtKey].delta.confirmed);
+            stateObject.deaths += (stateData.districtData[districtKey].deceased + stateData.districtData[districtKey].delta.deceased);
+            stateObject.active += stateData.districtData[districtKey].active;
+            stateObject.recovered += (stateData.districtData[districtKey].recovered + stateData.districtData[districtKey].delta.recovered);
+        });
+
+        return stateObject;
     }
 
     onExpand = (state: string) => {
